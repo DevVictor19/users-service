0e85f435d8e01c75d602897e816ba5ecedf6930d
DevVictor19 <antoniovictor12@live.com>
2026-01-04 17:44:06 -0300

    feat: enhance context handling with request ID and IP address retrieval

--- pkg/utils/http.go
+++ pkg/utils/http.go
@@ -1,8 +1,44 @@
 package utils
 
-import "github.com/labstack/echo/v4"
+import (
+	"context"
+	"time"
+	"usersservice/pkg/logger"
+
+	"github.com/labstack/echo/v4"
+)
+
+// ReqIDCtxKey is a key used for the Request ID in context
+type ReqIDCtxKey struct{}
+
+// Get ctx with timeout and request id from echo context
+func GetCtxWithReqID(c echo.Context) (context.Context, context.CancelFunc) {
+	ctx, cancel := context.WithTimeout(c.Request().Context(), time.Second*15)
+	ctx = context.WithValue(ctx, ReqIDCtxKey{}, GetRequestID(c))
+	return ctx, cancel
+}
+
+// Get context  with request id
+func GetRequestCtx(c echo.Context) context.Context {
+	return context.WithValue(c.Request().Context(), ReqIDCtxKey{}, GetRequestID(c))
+}
 
 // Get request id from echo context
 func GetRequestID(c echo.Context) string {
 	return c.Response().Header().Get(echo.HeaderXRequestID)
+}
+
+// Get user ip address
+func GetIPAddress(c echo.Context) string {
+	return c.Request().RemoteAddr
+}
+
+// Error response with logging error for echo context
+func LogResponseError(ctx echo.Context, logger logger.Logger, err error) {
+	logger.Errorf(
+		"ErrResponseWithLog, RequestID: %s, IPAddress: %s, Error: %s",
+		GetRequestID(ctx),
+		GetIPAddress(ctx),
+		err,
+	)
 }
